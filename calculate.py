d0aa32796628e8a347ea3554cce524e9b77fe09f
minipeck <sofiray31@gmail.com>
2024-11-14 09:21:57 +0300

    Внесены скорее всего последние изменения для соответствия формату PEP-8.

--- calculate.py
+++ calculate.py
@@ -10,7 +10,8 @@ def calc(fig, func, size):
     assert fig in figs, f"Unknown figure: {fig}"
     assert func in funcs, f"Unknown function: {func}"
 
-    result = eval(f'{fig}.{func}(*{size})', {'circle': circle, 'square': square})
+    result = eval(f'{fig}.{func}(*{size})',
+                  {'circle': circle, 'square': square})
     return result
 
 
@@ -20,15 +21,16 @@ if __name__ == "__main__":
     size = list()
 
     while fig not in figs:
-        fig = input(f"Enter figure name, available are {figs}:\n")
+        fig = input(f"Enter figure name, available are {figs}: \n")
 
     while func not in funcs:
-        func = input(f"Enter function name, available are {funcs}:\n")
+        func = input(f"Enter function name, available are {funcs}: \n")
 
     while len(size) != sizes.get(f"{func}-{fig}", 1):
         size = list(
             map(int, input(
-                "Input figure sizes separated by space, 1 for circle and square\n"
+                "Input figure sizes "
+                "separated by space, 1 for circle and square\n"
             ).split(' '))
         )
 
